67b0e65cd82954c7122f6c2d1ff40e097384869b
Joao Paulo <joaopaulosc06@gmail.com>
2020-10-15 14:03:00 -0300

    Mobile em desenvolvimento

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+
+const { Navigator, Screen} = createStackNavigator();
+
+export default function Routes() {
+    return ( 
+        <NavigationContainer >
+            <Navigator screenOptions={{headerShown: false}}>
+                <Screen 
+                name="OrphanagesMap" 
+                component={OrphanagesMap}
+                //desabilitar o header para cada pagina:
+                //options={{
+                //    headerShown: false
+                //}}
+                />
+
+                <Screen
+                    name="OrphanageDetails"
+                    component={OrphanageDetails}
+                />
+            </Navigator>
+        </NavigationContainer>
+
+    );
+}
